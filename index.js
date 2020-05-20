a1caea4b4df02a349a8f9df40d671bf06225b86d
jiayi-lim <65161176+jiayi-lim@users.noreply.github.com>
2020-05-20 15:09:23 +0800

    nussu spelling error

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ bot.onText(/\/submitnussu/, function (msg) {
           idRef.once('value', function(snapshot) {
             if (snapshot.hasChild(personid)) {
               idRef.child(personid).update({
-                ussu:url
+                nussu:url
               });
               bot.sendMessage(answer.chat.id, "NUSSU Survey proof received!");
             }
@@ -156,6 +156,19 @@ bot.onText(/\/submitfaculty/, function (msg) {
 
 
 // Feature 3: Queue
+// create queueRef in firebase
+const queueRef = sitesRef.child("queueDetails");
+queueRef.set({
+  currServing: 0,
+  currQueueNum: 0
+});
+// currServing: firebase --> tele  
+let currServing;
+queueRef.on('value', function(snapshot) {
+  currServing = snapshot.val().currServing;
+});
+// currServing: web --> firebase!!! 
+
 let currQueueNum = 0;
 bot.onText(/\/queue/, (msg) => {
   console.log("currQueueNum", currQueueNum);
@@ -163,17 +176,142 @@ bot.onText(/\/queue/, (msg) => {
   const id = msg.from.id;
   idRef.child(id).once('value', function(snapshot) {
     const userDetails = snapshot.val();
-    if (userDetails.queueNum != undefined) {
-      bot.sendMessage(id, "You are already in the queue. Your current queue numeber is " + 
-        snapshot.val().queueNum);
+    if (userDetails.collected) {
+      bot.sendMessage(id, "You have already collected the welfare pack.");
+    } else if (userDetails.queueNum != undefined) {
+      bot.sendMessage(id, "You are already in the queue. Your current queue number is " + 
+        userDetails.queueNum)
+        // .then(() => notif(userDetails.queueNum, id))
     } else if (userDetails.nussu == undefined || userDetails.faculty == undefined) {
       bot.sendMessage(id, "You have not completed the necessary surveys and forms.");
     } else {
-      currQueueNum = currQueueNum++;
-      bot.sendMessage(id, "Your queue number is " + currQueueNum.toString() + ".");
+      currQueueNum++;
+      // update user's q num
       idRef.child(id).update({
-        queueNum : currQueueNum
-      });
+        queueNum: currQueueNum
+      }).then(()=> {
+      // update the q details 
+      queueRef.update({
+        currQueueNum: currQueueNum
+      })}).then(()=> {
+      bot.sendMessage(id, "Your queue number is " + currQueueNum.toString() + 
+      ". We will notify you when your turn is near.")
+      // .then(() => notif(userDetails.queueNum, id))
+      })
+    }
+    console.log(userDetails)
+  });
+  // idRef.child(id).once('value', function(snapshot) {
+  //   const newuserDetails = snapshot.val();})
+  // console.log(newuserDetails, "new")
+});
+
+
+
+
+// Feature 4: Notify user when their turn is near
+function notif(queueNum,id) {
+  console.log(queueNum,currServing)
+  var status = true
+  while (status) {
+    if ((queueNum - currServing) <= 3) {
+      console.log("fulfilled condi")
+      status = false
+      bot.sendMessage(id, "Your turn is near, please head to the collection venue.")
+    }
+  }
+}
+
+function change(queueNum,id){
+  var old = currServing // assuming that the old will not change when currServing changes
+  if (old = currServing - 1) {
+    return true
+  }
+}
+
+function notif(queuenum,id){
+  var old = currServing // assuming that the old will not change when currServing changes
+  if (old = currServing - 1) { //currServing increased by 1 
+    if (1 <= (queueNum - currServing) <= 3) {
+      var number = queueNum - currServing
+      bot.sendMessage(id, "Your turn is near, only " + number.toString() + " students infront of you. please head to the collection venue.")
+      old = currServing + 1
+      notif(queueNum,id) //recurse
+    }
+    else if ((queueNum - currServing) == 0) {
+      return
+    }
+  }
+
+/// Feature 5: Check the number of ppl infront of them 
+bot.onText(/\/checkqueue/, (msg) => {
+  const id = msg.from.id;
+
+  idRef.child(id).once('value', function(snapshot) {
+    const details = snapshot.val();
+    if (details.collected) {
+      bot.sendMessage(id, "You have already collected the welfare pack.");
+    } else if (details.queueNum == undefined) {
+      bot.sendMessage(id, "You do not have a queue number yet.");
+    } else {
+      const num = details.queueNum - currServing - 1;
+      bot.sendMessage(id, "There are " + num.toString() + " people infront of you.");
     }
   });
 });
+
+
+
+/// Feature 6: Provide information about the welfare pack event. 
+// bot.onText(/\/info/, (msg) =>{
+//   const location = ;
+//   const date = ;
+//   const time = ;
+//   bot.sendMessage(msg.chat.id,  )
+// } )
+
+
+/// Feature 7*: Choose the snacks they want
+// bot.onText(/\/submit/, (msg) => {
+//     bot.sendMessage(msg.chat.id,'Okay, which survey?', {
+//       reply_markup: {
+//         inline_keyboard: [[
+//           {
+//             text: 'NUSSU',
+//             callback_data: 'nussu'
+//           },{
+//             text: 'Faculty',
+//             callback_data: 'faculty'
+//           }
+//         ]]
+//       }
+//     });
+//   });
+//   survey = callbackQuery.data
+//   chat_id = callbackQuery.message.chat.id 
+//   message_id = callbackQuery.message.message_id
+//   bot.deleteMessage(chat_id.toString(), message_id.toString())
+//   bot.sendMessage(chat_id, "Send me the photo.");
+//   // getsurvey()
+// function getsurvey() {
+//   bot.on('message', async (msg) => {
+//     if (msg.photo && msg.photo[0]) {
+//       bot.sendMessage(msg.chat.id, "Photo Received!")
+//       const personid = msg.from.id.toString()
+//       const file_id = msg.photo[0].file_id
+//       const fileinfo = await bot.getFile(file_id)
+//       const {file_path} = fileinfo
+//       const url = "https://api.telegram.org/file/bot" + "1140161041:AAFcapOrmPbMdyEdLY9azOhB-Nt8LJoLyqU" + "/" + file_path
+//       console.log(url)
+//       idRef.once('value', function(snapshot) {
+//         if (snapshot.hasChild(personid)) {
+//             if (survey == "nussu") {
+//               idRef.child(personid).update({
+//                 nussu:url
+//               })}
+//             else if (survey == "faculty") {
+//               idRef.child(personid).update({
+//                 faculty:url
+//               })}
+//             }})
+//           }})}
